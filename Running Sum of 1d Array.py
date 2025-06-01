973553ea8b676e43681c3365d2ccb301952a119a
Yashswijain85 <137017545+Yashswijain85@users.noreply.github.com>
2025-06-01 21:36:46 +0530

    Update Running Sum of 1d Array.py

--- Running Sum of 1d Array.py
+++ Running Sum of 1d Array.py
@@ -25,3 +25,12 @@ class Solution:
 
         return nums
 
+# Approach 3 --> in-place -- same complexity as above 
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sum = 0 
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+
+        return nums
+
